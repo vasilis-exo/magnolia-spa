fdcc306e0344ee466379854b5838dc86b9a4d15b
Bartosz Staryga <bartosz.staryga@magnolia-cms.com>
2020-04-02 21:56:52 +0200

    Use native wc implementation

--- magnolia/light-modules/web-components-minimal-lm/webresources/index.js
+++ magnolia/light-modules/web-components-minimal-lm/webresources/index.js
@@ -1,107 +1,63 @@
-import { LitElement, html } from 'https://unpkg.com/lit-element?module';
-import { unsafeHTML } from 'https://unpkg.com/lit-html@latest/directives/unsafe-html.js?module';
-
 // Headline
-class Headline extends LitElement {
-  static get properties() {
-    return { text: { type: String } };
+class Headline extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<h2 class="Headline">${this.text}</h2>`;
   }
 
-  constructor() {
-    super();
-    this.text = '';
-  }
-
-  render() {
-    return html`
-      <h2 class="Headline">${this.text}</h2>
-    `;
+  get text() {
+    return this.getAttribute('text');
   }
 }
 
 customElements.define('wc-headline', Headline);
 
 // Paragraph
-class Paragraph extends LitElement {
-  static get properties() {
-    return { richText: { type: String } };
+class Paragraph extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<p class="Paragraph">${this.richText}</p>`;
   }
 
-  constructor() {
-    super();
-    this.richText = '';
-  }
-
-  render() {
-    return html`
-      <p class="Paragraph">${unsafeHTML(this.richText)}</p>
-    `;
+  get richText() {
+    return this.getAttribute('richText');
   }
 }
 
 customElements.define('wc-paragraph', Paragraph);
 
 // Image
-class Image extends LitElement {
-  static get properties() {
-    return { src: { type: String } };
+class Image extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<img class="Image" src=${this.src} alt="Etiam Purus" />`;
   }
 
-  constructor() {
-    super();
-    this.src = '';
-  }
-
-  render() {
-    return html`
-      <img class="Image" src=${this.src} alt="Etiam Purus" />
-    `;
+  get src() {
+    return this.getAttribute('src');
   }
 }
 
 customElements.define('wc-image', Image);
 
 // List
-class List extends LitElement {
-  static get properties() {
-    return { items: { type: String } };
+class List extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<ul class="List">${this.items}</ul>`;
   }
 
-  constructor() {
-    super();
-    this.items = '';
-  }
-
-  render() {
-    return html`
-      <ul class="List">
-        ${unsafeHTML(this.items)}
-      </ul>
-    `;
-  }
-
-  createRenderRoot() {
-    return this;
+  get items() {
+    return this.getAttribute('items');
   }
 }
 
 customElements.define('wc-list', List);
 
 // listItem
-class listItem extends LitElement {
-  static get properties() {
-    return { text: { type: String } };
+class listItem extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<li class="Item">${this.text}</li>`;
   }
 
-  constructor() {
-    super();
-    this.text = '';
-  }
-
-  render() {
-    return html`
-      <li class="Item">${this.text}</li>
-    `;
+  get text() {
+    return this.getAttribute('text');
   }
 }
 
